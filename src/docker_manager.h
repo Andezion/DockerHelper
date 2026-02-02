5d64e021957e4dca7c14da44b3d2d57fa2c9be7f
Andezion <245122@edu.p.lodz.pl>
2026-02-02 12:17:19 +0100

    fixed some stuff

--- src/docker_manager.h
+++ src/docker_manager.h
@@ -4,24 +4,15 @@
 #include <wx/notebook.h>
 #include <wx/listctrl.h>
 #include <wx/timer.h>
-#include <string>
-
-struct ContainerInfo {
-    std::string id;
-    std::string name;
-    std::string status;
-    std::string image;
-};
-
-struct SystemInfo {
-    std::string cpu_usage;
-    std::string mem_usage;
-    int container_count;
-};
+#include <wx/thread.h>
+#include "docker_commands.h"
 
 class DockerManagerFrame : public wxFrame {
 public:
     DockerManagerFrame(const wxString& title);
+    ~DockerManagerFrame();
+    
+    void OnUpdateComplete(wxThreadEvent& event);
     
 private:
     wxNotebook* notebook;
@@ -38,6 +29,7 @@ private:
     wxStaticText* containersLabel;
     
     wxButton* stopButton;
+    wxButton* stopAllButton;
     wxButton* removeContainerButton;
     wxButton* removeImageButton;
     wxButton* removeVolumeButton;
@@ -45,16 +37,21 @@ private:
     wxButton* refreshButton;
     
     wxTimer* refreshTimer;
+    bool isUpdating;
     
-    std::string ExecuteCommand(const std::string& command);
-    void UpdateRunningContainers();
-    void UpdateStoppedContainers();
-    void UpdateUnusedImages();
-    void UpdateUnusedVolumes();
-    void UpdateSystemInfo();
-    void RefreshAll();
+    void CreateSystemInfoPanel(wxPanel* parent, wxSizer* sizer);
+    void CreateRunningPanel();
+    void CreateCleanupPanel();
+    
+    void PopulateRunningContainers(const std::vector<ContainerInfo>& containers);
+    void PopulateStoppedContainers(const std::vector<ContainerInfo>& containers);
+    void PopulateUnusedImages(const std::vector<ImageInfo>& images);
+    void PopulateUnusedVolumes(const std::vector<VolumeInfo>& volumes);
+    void UpdateSystemInfoUI(const SystemInfo& info);
+    void RefreshAllAsync();
     
     void OnStop(wxCommandEvent& event);
+    void OnStopAll(wxCommandEvent& event);
     void OnRemoveContainer(wxCommandEvent& event);
     void OnRemoveImage(wxCommandEvent& event);
     void OnRemoveVolume(wxCommandEvent& event);
@@ -67,15 +64,12 @@ private:
     void OnImageItemSelected(wxListEvent& event);
     void OnVolumeItemSelected(wxListEvent& event);
     
-    void CreateRunningPanel();
-    void CreateCleanupPanel();
-    void CreateSystemInfoPanel(wxPanel* parent, wxSizer* sizer);
-    
     wxDECLARE_EVENT_TABLE();
 };
 
 enum {
     ID_STOP = wxID_HIGHEST + 1,
+    ID_STOP_ALL,
     ID_REMOVE_CONTAINER,
     ID_REMOVE_IMAGE,
     ID_REMOVE_VOLUME,
@@ -85,7 +79,8 @@ enum {
     ID_RUNNING_LIST,
     ID_STOPPED_LIST,
     ID_IMAGES_LIST,
-    ID_VOLUMES_LIST
+    ID_VOLUMES_LIST,
+    ID_UPDATE_COMPLETE
 };
 
 class DockerManagerApp : public wxApp {
@@ -93,3 +88,4 @@ public:
     virtual bool OnInit();
 };
 
+wxDECLARE_EVENT(wxEVT_UPDATE_COMPLETE, wxThreadEvent);
